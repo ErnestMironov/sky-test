d4828540cf7fd4d637bd73d815948c6f3f88c86f
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-05-25 04:21:37 +0300

    Updates

---
+++ precache-manifest.8380f36bdff6e95c88da2ae02601cda4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe2c7ee02a3825a4594",
+    "url": "/sky-test/css/app.15844aa2.css"
+  },
+  {
+    "revision": "2274883f6217e2253c4e7a901b8edfd4",
+    "url": "/sky-test/index.html"
+  },
+  {
+    "revision": "2fe2c7ee02a3825a4594",
+    "url": "/sky-test/js/app.618db99a.js"
+  },
+  {
+    "revision": "7de22c650d2525a5de06",
+    "url": "/sky-test/js/chunk-vendors.f8390ff0.js"
+  },
+  {
+    "revision": "c2fcf50c14e720ac9395132859f90f7f",
+    "url": "/sky-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sky-test/robots.txt"
+  }
+]);
